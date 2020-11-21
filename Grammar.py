686211297202537d04d16dc75d883efda8308e8a
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-11-22 00:48:14 +0300

    Implement T0 grammar building algorithm Fix final states in tm.json Add some utils to Grammar

--- Grammar.py
+++ Grammar.py
@@ -10,6 +10,12 @@ class Production:
     head: List[Union[Terminal, Variable]]
     body: List[Union[Terminal, Variable]]
 
+    def __hash__(self):
+        return hash(tuple(self.head)) * 31 + hash(tuple(self.body))
+
+    def __repr__(self):
+        return f'{"".join(self.head)} -> {"".join(self.body)}'
+
 
 @dataclass
 class Grammar:
@@ -17,3 +23,6 @@ class Grammar:
     terminals: Set[Terminal]
     productions: Set[Production]
     start_variable: Variable
+
+    def __repr__(self):
+        return '\n'.join([str(prod) for prod in self.productions])
--- TuringMachine.py
+++ TuringMachine.py
@@ -1,25 +1,30 @@
 import json
 from dataclasses import dataclass
-from enum import Enum
+from itertools import product
 from pathlib import Path
 from typing import Set, Mapping, Tuple
+
+from Grammar import Grammar, Production
 
 State = str
 
 
 class Symbol(str):
     BLANK = '_'
+    EPS = 'eps'
 
 
-class Direction(Enum):
+class Direction:
     LEFT = '<'
     RIGHT = '>'
 
 
 @dataclass
 class TuringMachine:
+    # Assume that machine's "head" position is at the beginning of a word on the "tape"
     language_alphabet: Set[Symbol]
     tape_alphabet: Set[Symbol]
+    states: Set[State]
     start_state: State
     final_states: Set[State]
     transitions: Mapping[Tuple[State, Symbol], Tuple[State, Symbol, Direction]]
@@ -33,17 +38,49 @@ class TuringMachine:
             language_alphabet = set(data['language_alphabet'])
             tape_alphabet = language_alphabet.copy()
             transitions = {}
+            states = set()
             for key, value in data['transitions'].items():
                 state_from, symb_from = key.split(',')
                 state_to, symb_to, dir = value.split(',')
                 transitions[state_from, symb_from] = state_to, symb_to, dir
                 tape_alphabet.update({symb_from, symb_to})
-        return cls(language_alphabet, tape_alphabet, start_state, final_states, transitions)
+                states.update({state_from, state_to})
+        return cls(language_alphabet, tape_alphabet, states, start_state, final_states, transitions)
 
-    def to_grammar(self):
-        pass
+    def to_grammar(self) -> Grammar:
+        productions = set()
+        productions.add(Production(['A1'], [self.start_state, 'A2']))
+        for a in self.language_alphabet:
+            productions.add(Production(['A2'], [f'[{a},{a}]', 'A2']))
+        productions.add(Production(['A2'], ['A3']))
+        productions.add(Production(['A3'], [f'[{Symbol.EPS}, {Symbol.BLANK}]', 'A3']))
+        productions.add(Production(['A3'], [Symbol.EPS]))
+        language_alphabet_with_eps = self.language_alphabet.union({Symbol.EPS})
+        for a in language_alphabet_with_eps:
+            for (q, c), (p, e, d) in self.transitions.items():
+                if d == Direction.RIGHT:
+                    productions.add(Production([q, f'[{a},{c}]'], [f'[{a},{e}]', p]))
+        for a, b in product(language_alphabet_with_eps, language_alphabet_with_eps):  # with EPS or not?
+            for i in self.tape_alphabet:
+                for (q, c), (p, j, d) in self.transitions.items():
+                    if d == Direction.LEFT:
+                        productions.add(Production([f'[{b},{i}]', q, f'[{a},{c}]'], [p, f'[{b},{i}]', f'[{a},{j}]']))
+        for a in language_alphabet_with_eps:
+            for c in self.tape_alphabet:
+                for q in self.final_states:
+                    productions.add(Production([f'[{a},{c}]', q], [q, a, q]))
+                    productions.add(Production([q, f'[{a},{c}]'], [q, a, q]))
+        for q in self.final_states:
+            productions.add(Production([q], [Symbol.EPS]))
+        new_variables = set(f'[{v1},{v2}]' for v1, v2 in product(language_alphabet_with_eps, self.tape_alphabet))
+        return Grammar(
+            variables=new_variables.union(self.states).union({'A1', 'A2', 'A3'}),
+            terminals=self.language_alphabet,
+            productions=productions,
+            start_variable='A1'
+        )
 
 
 if __name__ == '__main__':
     tm = TuringMachine.from_json(Path('./tm.json'))
-    print(tm.language_alphabet)
+    print(tm.to_grammar())
