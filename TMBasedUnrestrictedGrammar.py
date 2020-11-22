4faf5675625a78fb4e8a1c8bef3da8fcfc928d37
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-11-22 18:13:13 +0300

    Add blank and directions as a setting Refactoring & code clean up

--- TMBasedUnrestrictedGrammar.py
+++ TMBasedUnrestrictedGrammar.py
@@ -3,49 +3,50 @@ from itertools import product
 from math import log2
 
 from Grammar import Production, Grammar
-from TuringMachine import Symbol, Direction, TuringMachine
+from TuringMachine import Symbol, Directions, TuringMachine
 
 
 class TMBasedUnrestrictedGrammar(Grammar):
     BLANKS_AT_THE_END_LIMIT = 2
 
-    def __init__(self, tm: TuringMachine):
+    def __init__(self, turing_machine: TuringMachine):
+        self.turing_machine = turing_machine
         productions = set()
 
         # Add generative productions with additional variables A1, A2 and A3
-        productions.add(Production(['A1'], [tm.start_state, 'A2']))
-        for a in tm.language_alphabet:
+        productions.add(Production(['A1'], [turing_machine.start_state, 'A2']))
+        for a in turing_machine.language_alphabet:
             productions.add(Production(['A2'], [f'[{a},{a}]', 'A2']))
         productions.add(Production(['A2'], ['A3']))
-        productions.add(Production(['A3'], [f'[{Symbol.EPS}, {Symbol.BLANK}]', 'A3']))
+        productions.add(Production(['A3'], [f'[{Symbol.EPS}, {turing_machine.blank}]', 'A3']))
         productions.add(Production(['A3'], [Symbol.EPS]))
 
         # Add checking productions which emulate the TM behaviour
-        language_alphabet_with_eps = tm.language_alphabet.union({Symbol.EPS})
+        language_alphabet_with_eps = turing_machine.language_alphabet.union({Symbol.EPS})
         for a in language_alphabet_with_eps:
-            for (q, c), (p, e, d) in tm.transitions.items():
-                if d == Direction.RIGHT:
+            for (q, c), (p, e, d) in turing_machine.transitions.items():
+                if d == Directions.RIGHT:
                     productions.add(Production([q, f'[{a},{c}]'], [f'[{a},{e}]', p]))
         for a, b in product(language_alphabet_with_eps, language_alphabet_with_eps):  # with EPS or not?
-            for i in tm.tape_alphabet:
-                for (q, c), (p, j, d) in tm.transitions.items():
-                    if d == Direction.LEFT:
+            for i in turing_machine.tape_alphabet:
+                for (q, c), (p, j, d) in turing_machine.transitions.items():
+                    if d == Directions.LEFT:
                         productions.add(Production([f'[{b},{i}]', q, f'[{a},{c}]'], [p, f'[{b},{i}]', f'[{a},{j}]']))
 
         # Add checking productions which transform a virtual "tape" to a terminal string
         for a in language_alphabet_with_eps:
-            for c in tm.tape_alphabet:
-                for q in tm.final_states:
+            for c in turing_machine.tape_alphabet:
+                for q in turing_machine.final_states:
                     productions.add(Production([f'[{a},{c}]', q], [q, a, q]))
                     productions.add(Production([q, f'[{a},{c}]'], [q, a, q]))
-        for q in tm.final_states:
+        for q in turing_machine.final_states:
             productions.add(Production([q], [Symbol.EPS]))
 
-        new_variables = set(f'[{v1},{v2}]' for v1, v2 in product(language_alphabet_with_eps, tm.tape_alphabet))
-        self._tm = tm
+        new_variables = \
+            set(f'[{v1},{v2}]' for v1, v2 in product(language_alphabet_with_eps, turing_machine.tape_alphabet))
         super().__init__(
-            variables=new_variables.union(tm.states).union({'A1', 'A2', 'A3'}),
-            terminals=tm.language_alphabet,
+            variables=new_variables.union(turing_machine.states).union({'A1', 'A2', 'A3'}),
+            terminals=turing_machine.language_alphabet,
             productions=productions,
             start_variable='A1'
         )
@@ -53,10 +54,10 @@ class TMBasedUnrestrictedGrammar(Grammar):
     def accepts(self, word: str) -> bool:
         # Scale up the amount of "[EPS,BLANK]" at the end and the beginning of the tape exponentially
         for blanks_amount in [2 ** p for p in range(int(log2(self.BLANKS_AT_THE_END_LIMIT)))]:
-            sentences = deque([[f'[{Symbol.EPS},{Symbol.BLANK}]'] * blanks_amount
+            sentences = deque([[f'[{Symbol.EPS},{self.turing_machine.blank}]'] * blanks_amount
                                + ['q0']
                                + [f'[{x},{x}]' for x in word]
-                               + [f'[{Symbol.EPS},{Symbol.BLANK}]'] * blanks_amount])
+                               + [f'[{Symbol.EPS},{self.turing_machine.blank}]'] * blanks_amount])
 
             # Leave only "checking" productions
             non_generative_productions = {p for p in self.productions
@@ -70,7 +71,7 @@ class TMBasedUnrestrictedGrammar(Grammar):
                 sent = sentences.popleft()
 
                 # Optimization: if there is already a final state on the tape, accept it without producing all terminals
-                if any([x in self._tm.final_states for x in sent]):
+                if any([x in self.turing_machine.final_states for x in sent]):
                     return True
 
                 if tuple(sent) in visited_sentences:
--- TuringMachine.py
+++ TuringMachine.py
@@ -7,11 +7,10 @@ State = str
 
 
 class Symbol(str):
-    BLANK = '_'
     EPS = 'eps'
 
 
-class Direction:
+class Directions:
     LEFT = '<'
     RIGHT = '>'
 
@@ -21,10 +20,12 @@ class TuringMachine:
     # Assume that machine's "head" position is at the beginning of a word on the "tape"
     language_alphabet: Set[Symbol]
     tape_alphabet: Set[Symbol]
+    blank: Symbol
+    directions: Directions
     states: Set[State]
     start_state: State
     final_states: Set[State]
-    transitions: Mapping[Tuple[State, Symbol], Tuple[State, Symbol, Direction]]
+    transitions: Mapping[Tuple[State, Symbol], Tuple[State, Symbol, Directions]]
 
     @classmethod
     def from_json(cls, path_to_json: Path):
@@ -32,9 +33,13 @@ class TuringMachine:
             data = json.load(json_file)
             start_state = data['start_state']
             final_states = data['final_states']
+            blank = data['blank']
+            directions = Directions()
+            directions.LEFT = data['directions']['left']
+            directions.RIGHT = data['directions']['right']
             language_alphabet = set(data['language_alphabet'])
             tape_alphabet = language_alphabet.copy()
-            tape_alphabet.add(Symbol.BLANK)
+            tape_alphabet.add(blank)
             transitions = {}
             states = set()
             for key, value in data['transitions'].items():
@@ -43,4 +48,4 @@ class TuringMachine:
                 transitions[state_from, symb_from] = state_to, symb_to, dir
                 tape_alphabet.update({symb_from, symb_to})
                 states.update({state_from, state_to})
-        return cls(language_alphabet, tape_alphabet, states, start_state, final_states, transitions)
+        return cls(language_alphabet, tape_alphabet, blank, directions, states, start_state, final_states, transitions)
