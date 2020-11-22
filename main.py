7e4cd23a74b67ed08ae4715c9c2ec9b3d391f3b7
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-11-22 16:37:59 +0300

    Add turing machine for unary multiplication Fix BFS algo Add some examples in the main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from TMBasedUnrestrictedGrammar import TMBasedUnrestrictedGrammar
 from TuringMachine import TuringMachine
 
 if __name__ == '__main__':
-    tm = TuringMachine.from_json(Path('./tm.json'))
-    t0_grammar = TMBasedUnrestrictedGrammar(tm)
-    print(t0_grammar.accepts('110'))
+    tm = TuringMachine.from_json(Path('tm.json'))
+    grammar = TMBasedUnrestrictedGrammar(tm)
+    assert grammar.accepts('11*11=1111') is True
+    assert grammar.accepts('111*1=111') is True
+    assert grammar.accepts('1*11=1') is False
+    assert grammar.accepts('=241*') is False
