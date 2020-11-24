676435e36825fb5f53c136a00eb82a053b93ad2e
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-11-24 20:37:34 +0300

    Rename UnrestrictedGrammar Remove blanks limit

--- UnrestrictedGrammar.py
+++ UnrestrictedGrammar.py
@@ -1,14 +1,11 @@
 from collections import deque
 from itertools import product
-from math import log2
 
 from Grammar import Production, Grammar
 from TuringMachine import Symbol, Directions, TuringMachine
 
 
-class TMBasedUnrestrictedGrammar(Grammar):
-    BLANKS_AT_THE_END_LIMIT = 2
-
+class UnrestrictedGrammar(Grammar):
     def __init__(self, turing_machine: TuringMachine):
         self.turing_machine = turing_machine
         productions = set()
@@ -52,38 +49,36 @@ class TMBasedUnrestrictedGrammar(Grammar):
         )
 
     def accepts(self, word: str) -> bool:
-        # Scale up the amount of "[EPS,BLANK]" at the end and the beginning of the tape exponentially
-        for blanks_amount in [2 ** p for p in range(int(log2(self.BLANKS_AT_THE_END_LIMIT)))]:
-            sentences = deque([[f'[{Symbol.EPS},{self.turing_machine.blank}]'] * blanks_amount
-                               + ['q0']
-                               + [f'[{x},{x}]' for x in word]
-                               + [f'[{Symbol.EPS},{self.turing_machine.blank}]'] * blanks_amount])
+        sentences = deque([[f'[{Symbol.EPS},{self.turing_machine.blank}]']
+                           + ['q0']
+                           + [f'[{x},{x}]' for x in word]
+                           + [f'[{Symbol.EPS},{self.turing_machine.blank}]']])
 
-            # Leave only "checking" productions
-            non_generative_productions = {p for p in self.productions
-                                          if p.head != ['A1'] and p.head != ['A2'] and p.head != ['A3']}
-            max_head_size = max([len(p.head) for p in non_generative_productions])
+        # Leave only "checking" productions
+        non_generative_productions = {p for p in self.productions
+                                      if p.head != ['A1'] and p.head != ['A2'] and p.head != ['A3']}
+        max_head_size = max([len(p.head) for p in non_generative_productions])
 
-            # Optimization: do not traverse sentences which were already visited
-            visited_sentences = set()
+        # Optimization: do not traverse sentences which were already visited
+        visited_sentences = set()
 
-            while len(sentences) > 0:
-                sent = sentences.popleft()
+        while len(sentences) > 0:
+            sent = sentences.popleft()
 
-                # Optimization: if there is already a final state on the tape, accept it without producing all terminals
-                if any([x in self.turing_machine.final_states for x in sent]):
-                    return True
+            # Optimization: if there is already a final state on the tape, accept it without producing all terminals
+            if any([x in self.turing_machine.final_states for x in sent]):
+                return True
 
-                if tuple(sent) in visited_sentences:
-                    continue
+            if tuple(sent) in visited_sentences:
+                continue
 
-                # Extending sentences
-                for substr_size in range(1, max_head_size + 1):
-                    for pos in range(len(sent) - substr_size + 1):
-                        prefix, substr, suffix = sent[:pos], sent[pos:pos + substr_size], sent[pos + substr_size:]
-                        for prod in non_generative_productions:
-                            if prod.head == substr:
-                                sentences.append(prefix + prod.body + suffix)
-                visited_sentences.add(tuple(sent))
+            # Extending sentences
+            for substr_size in range(1, max_head_size + 1):
+                for pos in range(len(sent) - substr_size + 1):
+                    prefix, substr, suffix = sent[:pos], sent[pos:pos + substr_size], sent[pos + substr_size:]
+                    for prod in non_generative_productions:
+                        if prod.head == substr:
+                            sentences.append(prefix + prod.body + suffix)
+            visited_sentences.add(tuple(sent))
 
         return False
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from pathlib import Path
 
-from TMBasedUnrestrictedGrammar import TMBasedUnrestrictedGrammar
+from UnrestrictedGrammar import UnrestrictedGrammar
 from TuringMachine import TuringMachine
 
 if __name__ == '__main__':
     tm = TuringMachine.from_json(Path('tm.json'))
-    grammar = TMBasedUnrestrictedGrammar(tm)
+    grammar = UnrestrictedGrammar(tm)
     assert grammar.accepts('11*11=1111') is True
-    assert grammar.accepts('111*1=111') is True
-    assert grammar.accepts('1*11=1') is False
-    assert grammar.accepts('=241*') is False
-    grammar.export(Path('./grammar.txt'))
