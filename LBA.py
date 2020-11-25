3714bc17a1ad35282f1d686a15f32a8452790631
Nikita Vlaev <nikitavlaev00@gmail.com>
2020-11-25 20:03:21 +0300

    Added lba to execute unary multiplication

---
+++ LBA.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from dataclasses import dataclass
+
+from TuringMachine import TuringMachine
+
+
+class BoundSymbol:
+    LEFT = '¢'
+    RIGHT = '$'
+
+@dataclass
+class LBA(TuringMachine):
+
+    @classmethod
+    def from_json(cls, path_to_json: Path):
+        lba = super(LBA, cls).from_json(path_to_json)
+        assert BoundSymbol.LEFT in lba.language_alphabet
+        assert BoundSymbol.RIGHT in lba.language_alphabet
+        return lba
