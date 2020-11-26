f8083ff694786cf478ea23626638f134822258f1
Nikita Vlaev <nikitavlaev00@gmail.com>
2020-11-26 21:38:04 +0300

    Added cozy 'export as json-like' function to TMBasedGrammar

--- run_csg.py
+++ run_csg.py
@@ -2,9 +2,7 @@ from ContextSensitiveGrammar import ContextSensitiveGrammar
 from LBA import LBA, BoundSymbol
 
 lba = LBA.from_json('lba.json')
-print(lba)
 
 csg = ContextSensitiveGrammar(lba)
-
-csg.export('csg.txt')
-print(csg.accepts('1111*1111=1111111111111111'))
+csg.export('t1.txt')
+print(csg.accepts('111*111=111111111'))
